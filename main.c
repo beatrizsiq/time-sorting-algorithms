1587e9a20dda0b7113a2d29abc1535dc60d18591
Beatriz Siqueira <88863619+beatrizsiq@users.noreply.github.com>
2022-05-31 22:25:19 -0300

    feat: :art: Adicionando o arquivo index.html (com bootstrap)

--- main.c
+++ main.c
@@ -2,31 +2,44 @@
 #include <stdlib.h>
 #include <time.h>
 
-main(){
-    printf("Ordenacao 500 posicoes:\n\n\n");
-    ordenacao_500_posicoes();
-    printf("\n\n\nOrdencao 5000 posicoes:\n\n\n");    
-    ordencao_5000_posicoes();
+main()
+{
+    FILE *html_arq;
+
+    html_arq = fopen("index.html", "w");
+
+    fprintf(html_arq, "<!DOCTYPE html><html><head><title>Tempo de Ordenação</title>");
+    fprintf(html_arq, "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\"rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">");
+    fprintf(html_arq, "</head><body>");
+    fprintf(html_arq, "<div><h1 class=\"display-1 bg-primary\">Oi<h1></div>");
+    fprintf(html_arq, "</body></html");
+
+    fclose(html_arq);
 }
 
-void ordenacao_500_posicoes(){
+
+
+void ordenacao_500_posicoes()
+{
     FILE *arquivo;
-    
+
     clock_t inicio_qsort, fim_qsort;
-    double tempo_qsort; 
-    
+    double tempo_qsort;
+
     int tam = 500;
     int vetor[tam];
     int i = 1;
 
     arquivo = fopen("numeros.txt", "r");
 
-    if (!arquivo){
+    if (!arquivo)
+    {
         printf("Problema ao abrir o arquivo!\n");
         return;
     }
 
-    while (i<=tam){
+    while (i <= tam)
+    {
         fscanf(arquivo, "%d", &vetor[i]);
         i++;
     }
@@ -34,38 +47,40 @@ void ordenacao_500_posicoes(){
     fclose(arquivo);
 
     heapSort(vetor, tam);
-    
-    inicio_qsort = clock();
-    quickSort(vetor,tam, 1, tam);
-    fim_qsort= clock();
-    tempo_qsort = (double)(fim_qsort - inicio_qsort) / CLOCKS_PER_SEC;
-	printf("\nTempo de execucao Quick Sort: %lf", tempo_qsort);
-	
-	shellSort(vetor, tam);
-    selectionSort(vetor, tam);
-	insertionSort(vetor, tam);
-	bubbleSort(vetor, tam);
 
+    inicio_qsort = clock();
+    quickSort(vetor, tam, 1, tam);
+    fim_qsort = clock();
+    tempo_qsort = (double)(fim_qsort - inicio_qsort) / CLOCKS_PER_SEC;
+    printf("\nTempo de execucao Quick Sort: %lf", tempo_qsort);
+
+    shellSort(vetor, tam);
+    selectionSort(vetor, tam);
+    insertionSort(vetor, tam);
+    bubbleSort(vetor, tam);
 }
 
-void ordencao_5000_posicoes(){
-	FILE *arquivo;
-    
+void ordencao_5000_posicoes()
+{
+    FILE *arquivo;
+
     clock_t inicio_qsort, fim_qsort;
-    double tempo_qsort; 
-    
+    double tempo_qsort;
+
     int tam = 5000;
     int vetor[tam];
     int i = 1;
 
     arquivo = fopen("numeros.txt", "r");
 
-    if (!arquivo){
+    if (!arquivo)
+    {
         printf("Problema ao abrir o arquivo!\n");
         return;
     }
 
-    while (i<=tam){
+    while (i <= tam)
+    {
         fscanf(arquivo, "%d", &vetor[i]);
         i++;
     }
@@ -73,31 +88,35 @@ void ordencao_5000_posicoes(){
     fclose(arquivo);
 
     heapSort(vetor, tam);
-    
+
     inicio_qsort = clock();
-    quickSort(vetor,tam, 1, tam);
-    fim_qsort= clock();
+    quickSort(vetor, tam, 1, tam);
+    fim_qsort = clock();
     tempo_qsort = (double)(fim_qsort - inicio_qsort) / CLOCKS_PER_SEC;
 
-	printf("\nTempo de execucao Quick Sort: %lf", tempo_qsort);
-	
-	shellSort(vetor, tam);
-	selectionSort(vetor, tam);
-	insertionSort(vetor, tam);
-	bubbleSort(vetor, tam);
+    printf("\nTempo de execucao Quick Sort: %lf", tempo_qsort);
+
+    shellSort(vetor, tam);
+    selectionSort(vetor, tam);
+    insertionSort(vetor, tam);
+    bubbleSort(vetor, tam);
 }
 
 // Métodos de Ordenação:
 
-void bubbleSort(int vet[], int TAM){
+void bubbleSort(int vet[], int TAM)
+{
     int i, j, aux;
     clock_t inicio, fim;
-    double tempo; 
+    double tempo;
 
     inicio = clock();
-    for (i = 0; i < TAM - 1; i++){
-        for (j = i + 1; j < TAM; j++){
-            if (vet[i] > vet[j]){
+    for (i = 0; i < TAM - 1; i++)
+    {
+        for (j = i + 1; j < TAM; j++)
+        {
+            if (vet[i] > vet[j])
+            {
                 aux = vet[i];
                 vet[i] = vet[j];
                 vet[j] = aux;
@@ -109,16 +128,19 @@ void bubbleSort(int vet[], int TAM){
     printf("\nTempo de Execucao Bubble Sort: %lf", tempo);
 }
 
-void insertionSort(int vet[], int TAM){
+void insertionSort(int vet[], int TAM)
+{
     int i, j, chave;
     clock_t inicio, fim;
-    double tempo; 
+    double tempo;
 
     inicio = clock();
-    for (i = 1; i < TAM; i++){
+    for (i = 1; i < TAM; i++)
+    {
         chave = vet[i];
         j = i - 1;
-        while (j >= 0 && vet[j] > chave){
+        while (j >= 0 && vet[j] > chave)
+        {
             vet[j + 1] = vet[j];
             j = j - 1;
         }
@@ -129,19 +151,23 @@ void insertionSort(int vet[], int TAM){
     printf("\nTempo de Execucao Insertion Sort: %lf", tempo);
 }
 
-void selectionSort(int vet[], int TAM){
+void selectionSort(int vet[], int TAM)
+{
     int i, j, min, aux;
     clock_t inicio, fim;
-    double tempo; 
+    double tempo;
 
     inicio = clock();
-    for (i = 0; i < (TAM - 1); i++){
+    for (i = 0; i < (TAM - 1); i++)
+    {
         min = i;
-        for (j = (i + 1); j < TAM; j++){
+        for (j = (i + 1); j < TAM; j++)
+        {
             if (vet[j] < vet[min])
                 min = j;
         }
-        if (i != min){
+        if (i != min)
+        {
             aux = vet[i];
             vet[i] = vet[min];
             vet[min] = aux;
@@ -152,78 +178,92 @@ void selectionSort(int vet[], int TAM){
     printf("\nTempo de Execucao Selection Sort: %lf", tempo);
 }
 
-void shellSort(int vet[], int TAM){
-	
-	int h = 1, valor, j, i;
+void shellSort(int vet[], int TAM)
+{
+
+    int h = 1, valor, j, i;
     clock_t inicio, fim;
-    double tempo; 
+    double tempo;
 
     inicio = clock();
-    do{
+    do
+    {
         h = (3 * h) + 1;
-    }while (h < TAM);
-    do{
+    } while (h < TAM);
+    do
+    {
         h /= 3;
-        for (i = h; i < TAM; i++){
+        for (i = h; i < TAM; i++)
+        {
             valor = vet[i];
             j = i - h;
-            while ((j >= 0) && (valor < vet[j])){
+            while ((j >= 0) && (valor < vet[j]))
+            {
                 vet[j + h] = vet[j];
                 j -= h;
             }
             vet[j + h] = valor;
         }
-     } while (h > 1);
-     
-	fim = clock();
+    } while (h > 1);
+
+    fim = clock();
     tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
     printf("\nTempo de Execucao ShellSort: %lf", tempo);
- }
+}
 
-void quickSort(int vetor[], int tam, int inicio, int fim){
+void quickSort(int vetor[], int tam, int inicio, int fim)
+{
     vetor[tam];
     int pivo, aux, i, j, meio;
 
     i = inicio;
     j = fim;
-    meio = (i+j) / 2;
+    meio = (i + j) / 2;
     pivo = vetor[meio];
-    do {
-        while(vetor[i] < pivo)
+    do
+    {
+        while (vetor[i] < pivo)
             i = i + 1;
-        while(vetor[j] > pivo)
+        while (vetor[j] > pivo)
             j = j - 1;
-        if (i <= j) {
+        if (i <= j)
+        {
             aux = vetor[i];
             vetor[i] = vetor[j];
             vetor[j] = aux;
             i = i + 1;
             j = j - 1;
         }
-    }while(j > i);
+    } while (j > i);
 
-    if(inicio < j) {
+    if (inicio < j)
+    {
         quickSort(vetor, tam, inicio, j);
     }
-    if(i < fim) {
+    if (i < fim)
+    {
         quickSort(vetor, tam, i, fim);
     }
-    
 }
 
-void heapSort(int vetor[], int TAM){
+void heapSort(int vetor[], int TAM)
+{
     int i, pai, filho, aux, n;
     clock_t inicio, fim;
-    double tempo; 
+    double tempo;
 
     inicio = clock();
-    i = TAM/2;
+    i = TAM / 2;
     n = TAM;
-    while(1) {
-        if (i > 0) {
+    while (1)
+    {
+        if (i > 0)
+        {
             i--;
             aux = vetor[i];
-        } else {
+        }
+        else
+        {
             n--;
             if (n == 0)
                 break;
@@ -231,19 +271,22 @@ void heapSort(int vetor[], int TAM){
             vetor[n] = vetor[0];
         }
         pai = i;
-        filho = (i*2) + 1;
-        while (filho < n) {
+        filho = (i * 2) + 1;
+        while (filho < n)
+        {
             if ((filho + 1 < n) && (vetor[filho + 1] > vetor[filho]))
                 filho++;
-            if (vetor[filho] > aux) {
+            if (vetor[filho] > aux)
+            {
                 vetor[pai] = vetor[filho];
                 pai = filho;
-                filho = (pai*2) + 1;
-            } else
+                filho = (pai * 2) + 1;
+            }
+            else
                 break;
         }
         vetor[pai] = aux;
-    }  
+    }
     fim = clock();
     tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
     printf("Tempo de Execucao HeapSort: %lf", tempo);
